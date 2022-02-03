4529a250e756cc659bbd8b0b392d4edf4d996f87
wighawag <wighawag@gmail.com>
2022-02-03 19:44:00 +0000

    setup

---
+++ documentation/src/layouts/default.svelte
@@ -0,0 +1,21 @@
+<script>  
+  // import {routes} from '../metadata'
+  
+  export let title
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<nav>
+  <ul>
+    <!-- {#each routes as route}
+      <li>
+        <a href={route.path}>{route.label}</a>
+      </li>
+    {/each} -->
+  </ul>
+</nav>
+
+<slot />
---
+++ documentation/svelte.config.js
@@ -0,0 +1,19 @@
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
+import adapter from '@sveltejs/adapter-netlify'
+
+const config = {
+	kit: {
+		adapter: adapter(), // currently the adapter does not take any options
+		prerender: {
+			crawl: true,
+			enabled: true,
+			onError: 'continue',
+			entries: ['*'],
+		},
+	},
+	extensions: [".svelte", ...mdsvexConfig.extensions],
+	preprocess: [mdsvex(mdsvexConfig)]
+};
+
+export default config;
